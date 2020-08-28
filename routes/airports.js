1ad7246af07fc58839ef03962d5a11c020287186
Chris <chris.hoffmann84@gmail.com>
2020-08-28 22:47:13 +0200

    added airports-test view, implemented airport filtering

--- routes/airports.js
+++ routes/airports.js
@@ -4,10 +4,36 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  //res.render('index', { title: 'YAPF - Yet Another Plane Finder' });
-  let airportdata = new AirportData();
-  res.send(airportdata.dump("EDDK"));
+router.get('/', function(req, res, next) 
+{
+    let airportdata = new AirportData();
+
+    let originIcao = "EDDK";
+    let destinationIcao = "EDDM";
+
+    let origin = airportdata.getAirport(originIcao);    
+    let destination = airportdata.getAirport(destinationIcao);
+
+    let distance = airportdata.calcDistanceLatLong(origin.lat, origin.lon, destination.lat, destination.lon);
+
+    let maxDistance = 125;
+    let airportsInRange = airportdata.airportsInRange(originIcao, maxDistance);
+
+    res.render('airports', 
+        {
+            title: "Airports-Test",
+            origin: originIcao,
+            originLat: origin.lat,
+            originLong: origin.lon,
+            destination: destinationIcao, 
+            destinationLat: destination.lat,
+            destinationLong: destination.lon,
+            distance: distance,
+
+            maxDistance: maxDistance,
+            airportsInRange: airportsInRange
+        }
+    );
 });
 
 module.exports = router;
--- routes/utils/airportdata.js
+++ routes/utils/airportdata.js
@@ -7,10 +7,69 @@ class AirportData
         console.log("ICAOS.length: " + ICAOS.length);
     }
 
+    getAirport(icao)
+    {
+        return ICAOS.find((e) => e.icao == icao.toUpperCase());
+    }
+
     dump(icao)
     {
-        let airport = ICAOS.find((e) => e.icao = icao.toUpperCase());
+        let airport = this.getAirport(icao);
         return JSON.stringify(airport);
+    }
+
+    calcDistanceIcaoIcao(originIcao, destinationIcao)
+    {
+
+        let origin = this.getAirport(originIcao);
+        if (!origin) return -1;
+
+        return this.calcDistanceLatLongIcao(origin.lat, origin.lon, destinationIcao);
+    }
+
+    calcDistanceLatLongIcao(originLat, originLong, destinationIcao)
+    {
+        let destination = this.getAirport(destinationIcao);
+        if (!destination) return -1;
+
+        return this.calcDistanceLatLong(originLat, originLong, destination.lat, destination.lon);
+    }
+
+    calcDistanceLatLong(originLat, originLong, destinationLat, destinationLong)
+    {
+        if (originLat == destinationLat && originLong == destinationLong)
+        {
+            return 0;
+        }
+
+        let lat1 = originLat * (Math.PI/180);
+        let long1 = originLong * (Math.PI/180);
+        let lat2 = destinationLat * (Math.PI/180);
+        let long2 = destinationLong * (Math.PI/180);
+
+        let sinLat1 = Math.sin(lat1);
+        let sinLat2 = Math.sin(lat2);
+        let cosLat1 = Math.cos(lat1);
+        let cosLat2 = Math.cos(lat2);
+
+        let distanceRadians = Math.acos(sinLat1 * sinLat2 + cosLat1 * cosLat2 * Math.cos(long2 - long1));
+
+        let distance =  3443.9 * distanceRadians;
+
+        return distance;
+    }
+
+    airportsInRange(originIcao, maxDistance)
+    {
+        let origin = this.getAirport(originIcao);
+        if (!origin) return [];
+
+        console.log("Enter airportsInRange(originIcao, maxDistance)...");
+        let begin = new Date().getTime();
+        let inRange = ICAOS.filter((airport) => this.calcDistanceLatLongIcao(origin.lat, origin.lon, airport.icao) <= maxDistance && origin.icao != airport.icao);
+        console.log("Finished airportsInRange(originIcao, maxDistance)! Duration: " + (new Date().getTime() - begin) + "ms");
+        
+        return inRange;
     }
 }
 
